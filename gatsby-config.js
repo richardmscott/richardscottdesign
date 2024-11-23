f30dfc33d42072ba33ffcba45a946813c3a835fa
RICHARD SCOTT <richardscott@richards-air.home>
2024-11-23 15:01:13 +0000

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
 module.exports = {
   siteMetadata: {
     title: `Richard Scott Design`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://rscottdesign.co.uk`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
 const pageStyles = {
   color: "#232129",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: "-apple-system, Arial, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
